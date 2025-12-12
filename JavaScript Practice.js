132d76d9a7fd926dde4021d6f5142213cd981cba
trebman95 <trebman91@hotmail.com>
2025-12-11 21:54:22 -0500

    first commit

---
+++ JavaScript Practice.js
@@ -0,0 +1,10 @@
+// Data Types, String Contatenation, Properties, & Built-In Objects
+console.log('JavaScript');
+console.log(2011);
+console.log('Woohoo I love to code! #codecademy');
+console.log(20.49);
+console.log('Hello' + ' ' + 'World');
+console.log('Teaching the world how to code'.length);
+console.log("Codecademy".toUpperCase());
+console.log(Math.floor(Math.random() * 100));
+console.log(Number.isInteger(2017));
