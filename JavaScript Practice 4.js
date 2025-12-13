b14648eb8e1f55db1bda1bf3d04bb1a771d4e9f5
trebman95 <trebman91@hotmail.com>
2025-12-13 18:02:50 -0500

    JavaScript Practice 5

--- JavaScript Practice 4.js
+++ JavaScript Practice 4.js
@@ -1,8 +1,102 @@
-/// Conditionals (if, if...else,)
+/// Conditionals (if, if...else, Logical Operators, Truthy & Falsy, Ternary Operator, Else if, Switch)
 let sale = true;
 sale = false;
 if (sale) {
     console.log('Time to buy!')
 } else {
     console.log('Time for a sale!')
+}
+
+let hungerLevel = 7;
+if (hungerLevel > 7) {
+    console.log('Time to eat!')
+} else {
+    console.log('We can eat later!')
+}
+
+let mood = 'sleepy';
+let tirednessLevel = 6;
+
+if (mood === 'sleepy' || tirednessLevel > 8) {
+    console.log('time to sleep')
+} else {
+    console.log('not bedtime yet')
+}
+
+let sleep = 'sleepy';
+let tired = 6;
+
+if (sleep === 'sleepy' && tired > 8) {
+    console.log('time to sleep')
+} else {
+    console.log('not bedtime yet')
+}
+
+let wordCount = 3; //Truthy
+
+if (wordCount) {
+    console.log("Great! You've started your work!");
+} else {
+    console.log('Better get to work!');
+}
+
+
+let favoritePhrase = ''; //Falsy
+
+if (favoritePhrase) {
+    console.log("This string doesn't seem to be empty.");
+} else {
+    console.log('This string is definitely empty.');
+}
+
+let tool = '';
+
+// Use short-circuit evaluation to assign  writingUtensil variable below:
+let writingUtensil = tool || 'pen'
+console.log(`The ${writingUtensil} is mightier than the sword.`);
+
+//Ternary Operator to summarize condition statements with ? OR :
+let isLocked = false;
+isLocked ? console.log('You will need a key to open the door.') : console.log('You will not need a key to open the door.');
+
+
+let isCorrect = true;
+isCorrect ? console.log('Correct!') : console.log('Incorrect!');
+
+
+let favoriteSay = 'Love That!';
+favoriteSay === 'Love That!' ? console.log('I love that!') : console.log("I don't love that!");
+
+let season = "summer";
+
+if (season === "spring") {
+    console.log("It's spring! The trees are budding!");
+} else if (season === "winter") {
+    console.log("It's winter! Everything is covered in snow.");
+} else if (season === "fall") {
+    console.log("It's fall! Leaves are falling!");
+} else if (season === "summer") {
+    console.log("It's sunny and warm because it's summer!");
+} else {
+    console.log("Invalid season.");
+}
+
+
+// Switch keyword
+
+let athleteFinalPosition = "first place";
+
+switch (athleteFinalPosition) {
+    case "first place":
+        console.log("You get the gold medal!");
+        break;
+    case "second place":
+        console.log("You get the silver medal!");
+        break;
+    case "third place":
+        console.log("You get the bronze medal!");
+        break;
+    default:
+        console.log("No medal awarded.");
+        break;
 }
