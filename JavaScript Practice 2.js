da4afebfde91f1daf1aa8b7c2e38423e31d562c2
trebman95 <trebman91@hotmail.com>
2025-12-13 13:26:44 -0500

    JS Practice 2 2nd try

--- JavaScript Practice 2.js
+++ JavaScript Practice 2.js
@@ -1,3 +1,4 @@
+///////////////////////////////////////FIRST Try/////////////////////////
 // Forecast today is 293 Kelvin
 const kelvin = 293;
 // Converting Kelvin to Celsius
@@ -15,3 +16,21 @@ let newton = celsius * (33 / 100);
 newton = Math.floor(newton);
 //String Interpolation for Newton
 console.log(`The temperature is also ${newton} degrees Newton`);
+
+
+/*
+//////////////////////////////////////////CLEANER VERSION/////////////
+
+// Forecast today is 293 Kelvin
+const kelvin = 293;
+// Celsius = Kelvin - 273
+const celsius = kelvin - 273;
+// Farenheit = celsius * (9 / 5) + 32
+const farenheit = Math.floor(celsius * (9 / 5) + 32);
+console.log(`The temeperature is ${farenheit} degrees Farenheit.`);
+
+// Newton = celsius * (33 / 100)
+const newton = Math.floor(celsius * (33 / 100));
+console.log(`The temperature is also ${newton} degrees Newton`);
+
+*/
