973bff1ff35974125f2a40444b0a641add06195b
trebman95 <trebman91@hotmail.com>
2025-12-13 13:18:41 -0500

    JS Practice 2 First Try

---
+++ JavaScript Practice 2.js
@@ -0,0 +1,17 @@
+// Forecast today is 293 Kelvin
+const kelvin = 293;
+// Converting Kelvin to Celsius
+let celsius = kelvin - 273;
+//Calculate Farenheit
+let farenheit = celsius * (9 / 5) + 32;
+//Converting Celsius to Farenheit
+farenheit = Math.floor(farenheit);
+//String Interpolation for Farenheit
+console.log(`The temeperature is ${farenheit} degrees Farenheit.`);
+
+//Convert Celsius to the Newton scale
+let newton = celsius * (33 / 100);
+//Round down the Newton Temperature
+newton = Math.floor(newton);
+//String Interpolation for Newton
+console.log(`The temperature is also ${newton} degrees Newton`);
