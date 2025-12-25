11b316f822f23aaa30eda2775a8b352030aee0f7
trebman95 <trebman91@hotmail.com>
2025-12-24 22:59:41 -0500

    Project 3 added

--- Practice Project 3.js
+++ Practice Project 3.js
@@ -26,3 +26,29 @@ const gamePrices = (game) => {
 
 console.log(gamePrices('Madden'));
 console.log(gamePrices(20.47));
+
+
+
+
+const listOfGames = ['Fortnite', 'The Last of Us', 'Madden 2025', 'NBA 2K11', 'FIFA 2013', 'Star Wars Jedi: Fallen Order']
+
+/*
+console.log(listOfGames.slice(0, 2));
+console.log(listOfGames.shift());
+console.log(listOfGames.pop());
+console.log(listOfGames.length);
+console.log(listOfGames.slice(2, 4));
+
+listOfGames.unshift('Gears of War');
+console.log(listOfGames)
+*/
+
+const addingGames = (arr) => {
+    if (Array.isArray(arr)) {
+        arr.unshift('Gears of War')
+    }
+
+    return arr
+}
+
+console.log(addingGames(listOfGames))
