54c959e8edf7c16375ec4267d96d8346e377030e
trebman95 <trebman91@hotmail.com>
2025-12-24 20:58:00 -0500

    Added functions

--- Functions.js
+++ Functions.js
@@ -8,17 +8,33 @@ function greetInSpanish() {
     console.log('Buenas tardes.')
 }
 
+function favoriteFood() {
+    const food = 'tacos';
+    console.log(`It's all about dem ${food}!`)
+}
+
 getReminder();
 greetInSpanish();
+favoriteFood();
 
 
-let name = 'Tre'
+let name = 'Tre'           // Reassiging parameters
 
 function sayThanks(name) {
     console.log('Thank you for your purchase, ' + name + '! We appreciate your business.');
 }
 
 sayThanks('Cole')
+
+
+let dog = 'Pablo'
+
+function sayDogsName(dog) {
+    console.log(`My dogs name is ${dog}.`)
+}
+
+sayDogsName('Roscoe');
+
 
 
 function makeShoppingList(item1 = 'milk', item2 = 'bread', item3 = 'eggs') {   // Default parameters
@@ -30,6 +46,15 @@ function makeShoppingList(item1 = 'milk', item2 = 'bread', item3 = 'eggs') {   /
 makeShoppingList();
 
 
+function buyFancyClothes(shirt1 = 'Sean John', shirt2 = 'Roc-A-Wear', shirt3 = 'DKNY') {
+    console.log(`I'm rockin ${shirt1}`);
+    console.log(`I'm rockin ${shirt2}`);
+    console.log(`I'm rockin ${shirt3}`)
+}
+
+buyFancyClothes();
+
+
 function monitorCount(rows, columns) {
     return rows * columns
 }
@@ -38,16 +63,30 @@ const numOfMonitors = monitorCount(5, 4)
 console.log(numOfMonitors)
 
 
-function monitorCount(rows, columns) {   // Helper functions
-    return rows * columns;
-}
 
-function costOfMonitors(rows, columns) {
+function costOfMonitors(rows, columns) {      // Helper functions
     return monitorCount(rows, columns) * 200;
 }
 
 const totalCost = costOfMonitors(5, 4);
 console.log(totalCost)
+
+
+
+function newRoom(width, height) {
+    return width * height
+}
+
+const roomSize = newRoom(40, 50)
+console.log(roomSize)
+
+function costOfRoom(width, height) {
+    return newRoom(width, height) * 300;
+}
+
+
+
+
 
 
 const plantNeedsWater = function plantDay(day) {    // Function Expressions - NOT anonymous
@@ -61,6 +100,18 @@ const plantNeedsWater = function plantDay(day) {    // Function Expressions - NO
 console.log(plantNeedsWater('Tuesday'))
 
 
+const imHungry = function whatToEat(munchies) {
+    if (typeof munchies === "string") {
+        return 'Lets eat!'
+    } else {
+        return 'You are not really hungry.'
+    }
+}
+
+console.log(imHungry("burritos"));
+console.log(imHungry(17))
+
+
 
 const overWeight = (weight) => {      // Arrow functions
     if (weight > 200) {
@@ -72,7 +123,16 @@ const overWeight = (weight) => {      // Arrow functions
 
 console.log(overWeight(250))
 
+const findRadius = (diameter) => {
+    return diameter / 2
+}
+
+console.log(findRadius(3.14));
+
 
 
 const takingPills = day => day === 'Wednesday' ? true : false;    // Concise Arrow Body fucntions
 console.log(takingPills('Friday'))
+
+const takingMedication = weekday => weekday === 'Friday' ? 'Thank You' : 'Maybe another day';
+console.log(takingMedication('Tuesday'));
