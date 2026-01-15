23e7d3fa83dc34c14e32705959e02a7a121411b9
trebman95 <trebman91@hotmail.com>
2026-01-15 06:47:38 -0500

    Class Part 1

--- JS Algorithims/Classes & Modules.js
+++ JS Algorithims/Classes & Modules.js
@@ -24,3 +24,49 @@ console.log(halley.behavior);
 halley.incrementBehavior();
 console.log(halley.name);
 console.log(halley.behavior);
+
+
+
+class Surgeon {        // Instances - new
+    constructor(name, department) {
+        this.name = name;
+        this.department = department;
+    }
+}
+
+const surgeonRomero = new Surgeon('Francisco Romero', 'Cardiovascular')
+const surgeonJackson = new Surgeon('Ruth Jackson', 'Orthopedics')
+console.log(surgeonRomero.name)
+console.log(surgeonJackson.department)
+
+
+
+class Doctor {     // Methods
+    constructor(name, department) {
+        this._name = name;
+        this._department = department;
+        this._remainingVacationDays = 20;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    get department() {
+        return this._department;
+    }
+
+    get remainingVacationDays() {
+        return this._remainingVacationDays
+    }
+
+    takeVacationDays(daysOff) {
+        this._remainingVacationDays -= daysOff;
+    }
+}
+
+const doctorJames = new Doctor('James Brown', 'Cardiovascular');
+const doctorHendrix = new Doctor('Jimi Hendrix', 'Orthopedics');
+console.log(doctorHendrix.name);
+doctorHendrix.takeVacationDays(12);
+console.log(doctorHendrix.remainingVacationDays);
