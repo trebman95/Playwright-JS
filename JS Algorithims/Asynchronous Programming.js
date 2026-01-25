3140b8d53f6bae28b800aaf324dc39087ce5c51e
trebman95 <trebman91@hotmail.com>
2026-01-25 12:57:49 -0500

    Add Temu TC

--- JS Algorithims/Asynchronous Programming.js
+++ JS Algorithims/Asynchronous Programming.js
@@ -30,3 +30,39 @@ const usingSTO = () => {
 setTimeout(usingSTO, 2000);
 
 console.log("This is the last line of code in app.js.");
+
+
+
+
+function withConstructor(num) {       // async function
+    return new Promise((resolve, reject) => {
+        if (num === 0) {
+            resolve("zero");
+        } else {
+            resolve("not zero");
+        }
+    });
+}
+
+withConstructor(0).then((resolveValue) => {
+    console.log(
+        ` withConstructor(0) returned a promise which resolved to: ${resolveValue}.`
+    );
+});
+
+
+const withAsync = async (num) => {
+    if (num === 0) {
+        return 'zero'
+    } else {
+        return 'not zero'
+    }
+};
+
+withConstructor(0);
+
+
+withAsync(100)
+    .then((resolveValue) => {
+        console.log(` withAsync(100) returned a promise which resolved to: ${resolveValue}.`);
+    })
