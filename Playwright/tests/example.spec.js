e7e2a935f49eef0018c05637dd87b160de4bdaa3
trebman95 <trebman91@hotmail.com>
2026-01-27 21:37:42 -0500

    updated

--- Playwright/tests/example.spec.js
+++ Playwright/tests/example.spec.js
@@ -6,6 +6,8 @@ test('has title', async ({ page }) => {
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
+
+  await page.pause()
 });
 
 test('get started link', async ({ page }) => {
@@ -16,4 +18,6 @@ test('get started link', async ({ page }) => {
 
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+
+  await page.pause()
 });
